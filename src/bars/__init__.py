b44eec4f75c2e0b93f5a2acffc36a17d99526d96
Huberkeep <anithce@outlook.com>
2026-01-31 17:48:27 +0800

    Update __init__.py

--- src/bars/__init__.py
+++ src/bars/__init__.py
@@ -2,7 +2,8 @@ from .firatbars import ceicfirmbar,icrinputbar
 __all__ = ["ceicfirmbar","icrinputbar"]
 userInput = input("""Welcome!
 list:l
-more:m""")
-if UserInput == l:
+empty option:q""")
+if userInput == l:
   print("file firatbars.py")
-
+elif userInput == q:
+  pass
